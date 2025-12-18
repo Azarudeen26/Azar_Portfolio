55ee2931befe46abf1cc86d0ee4c18e3e942e8b4
Azar Deen <azar05311@gmail.com>
2025-12-18 21:22:48 +0530

    added script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("whatsappBtn").addEventListener("click", () => {
+  window.open("https://wa.me/919715189634?text=Hello Azarudeen, I visited your portfolio.", "_blank");
+});
+
+document.getElementById("resumeBtn").addEventListener("click", () => {
+  window.open("https://drive.google.com", "_blank");
+});
